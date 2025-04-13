f236c584a13a330ac44a67287b3bb448737ad83e
RewatkarVijay <er.vijayrewatkar@gmail.com>
2025-04-13 11:29:50 +0530

    Adding Files

---
+++ IAM_Users.tf
@@ -0,0 +1,48 @@
+resource "aws_iam_user" "user_1" {
+  name = "dt_Vijay"
+  path = "/"
+ 
+  tags = {
+    tag-key = "dt_Vijay-tagname"
+  }
+}
+
+resource "aws_iam_user" "user_2" {
+  name = "dt_Vishal"
+  path = "/system/"
+ 
+  tags = {
+    tag-key = "dt_Vishal-tagname"
+  }
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = "Demo-policy"
+  #description = "A Demo policy"
+  policy      = jsonencode(
+    {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:*",
+                "s3-object-lambda:*"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+)
+}
+
+
+resource "aws_iam_user_policy_attachment" "user_policy_attachment" {
+    user = aws_iam_user.lb.name
+    policy_arn = aws_iam_policy.policy.arn
+}
+
+resource "aws_iam_user_policy_attachment" "user_policy_attachment1" {
+    user = aws_iam_user.lb1.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform{
+  required_providers{
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  }
+
+    ## To create EC2 instace
+#resource "aws_instance" "example_server" {
+#  ami           = "ami-0d188df7cedce7d90"
+#  instance_type = "t2.micro"
+
+ # tags = {
+ #   Name = "E2QA1Web01"
+ # }
+#}
