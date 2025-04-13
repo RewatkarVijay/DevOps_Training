a63014cbeea3e5fca1a2fb36b795c74bf62c95db
RewatkarVijay <er.vijayrewatkar@gmail.com>
2025-04-13 11:48:44 +0530

    update lb to user1 and 2

--- IAM_Users.tf
+++ IAM_Users.tf
@@ -1,5 +1,4 @@
 
-    ## To create users
 resource "aws_iam_user" "user_1" {
   name = "dt_Vijay"
   path = "/"
@@ -19,7 +18,6 @@ resource "aws_iam_user" "user_2" {
 }
 
 
-    ## To create policy
 resource "aws_iam_policy" "policy" {
   name        = "Demo-policy"
   #description = "A Demo policy"
@@ -40,13 +38,12 @@ resource "aws_iam_policy" "policy" {
 )
 }
 
-    ## To attach policy to users
 resource "aws_iam_user_policy_attachment" "user_policy_attachment" {
-    user = aws_iam_user.lb.name
+    user = aws_iam_user.user_1.name
     policy_arn = aws_iam_policy.policy.arn
 }
 
 resource "aws_iam_user_policy_attachment" "user_policy_attachment1" {
-    user = aws_iam_user.lb1.name
+    user = aws_iam_user.user_2.name
     policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
 }
