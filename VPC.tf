46556b23ce3e6b9a8fc0ebe53e697f073c46036f
RewatkarVijay <er.vijayrewatkar@gmail.com>
2025-04-16 14:36:13 +0530

    create new pp subnet

--- VPC.tf
+++ VPC.tf
@@ -7,18 +7,18 @@ resource "aws_vpc" "test_vpc" {
   }
 }
 
-resource "aws_subnet" "test_subnet" {
-  vpc_id     = aws_vpc.test_vpc.id
-  cidr_block = "10.0.1.0/24"
+# resource "aws_subnet" "test_subnet" {
+#   vpc_id     = aws_vpc.test_vpc.id
+#   cidr_block = "10.0.1.0/24"
 
-  tags = {
-    Name = "test_subnet"
-  }
-}
+#   tags = {
+#     Name = "test_subnet"
+#   }
+# }
 
 resource "aws_subnet" "test_public_subnet" {
   vpc_id     = aws_vpc.test_vpc.id
-  cidr_block = "10.0.2.0/24"
+  cidr_block = "10.0.0.0/24"
 
   tags = {
     Name = "test_public_subnet"
@@ -27,7 +27,7 @@ resource "aws_subnet" "test_public_subnet" {
 
 resource "aws_subnet" "test_private_subnet" {
   vpc_id     = aws_vpc.test_vpc.id
-  cidr_block = "10.0.3.0/24"
+  cidr_block = "10.0.1.0/24"
 
   tags = {
     Name = "test_private_subnet"
