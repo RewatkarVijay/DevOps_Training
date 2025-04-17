3730a2cfed196aa981093ca00e16b2b31f1d0558
RewatkarVijay <er.vijayrewatkar@gmail.com>
2025-04-17 14:39:03 +0530

    add ec2 in vpc updated

--- EC2_Instance.tf
+++ EC2_Instance.tf
@@ -1,5 +1,5 @@
     ## To create EC2 instace
-resource "aws_instance" "example_server" {
+resource "aws_instance" "E2DEVWEB-01" {
  ami           = "ami-0af88851684f35a8c"
  instance_type = "t2.micro"
  subnet_id = aws_subnet.dev_private_subnet.id
@@ -10,7 +10,7 @@ resource "aws_instance" "example_server" {
 }
 
     ## To create EC2 instace
-resource "aws_instance" "example_server" {
+resource "aws_instance" "E2DEVWEB-02" {
  ami           = "ami-0af88851684f35a8c"
  instance_type = "t2.micro"
  subnet_id = aws_subnet.db_private_subnet.id
