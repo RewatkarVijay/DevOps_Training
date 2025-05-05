e7a5a27e63e09335e2faf53c2734f68a2863d62e
RewatkarVijay <er.vijayrewatkar@gmail.com>
2025-05-05 18:40:10 +0530

    add listener

--- main.tf
+++ main.tf
@@ -240,3 +240,17 @@ resource "aws_lb_target_group_attachment" "test2" {
   target_id        = aws_instance.ec2Test.id
   port             = 80
 }
+
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.front_end.arn
+  port              = "80"
+  protocol          = "HTTP"
+  #ssl_policy        = "ELBSecurityPolicy-2016-08"
+  #certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb-example.arn
+  }
+}
