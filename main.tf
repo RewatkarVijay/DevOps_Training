7cf5449bc58cd15d117fc1cc3e7a59271dfc6e03
RewatkarVijay <er.vijayrewatkar@gmail.com>
2025-05-07 11:05:22 +0530

    Update code

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "aws_eip" "nat_eip" {
+  instance = aws_instance.web-01.id
+  #domain   = "vpc"
+}
+ 
+# Create the NAT Gateway
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.q1_public_subnet.id
+  #vpc_id        = aws_vpc.main.id
+  tags = {
+    Name = "my-nat-gateway"
+  }
+}
+ 
