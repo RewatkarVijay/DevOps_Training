5a221b5065ebdf9b9132d4881b0e66c9a1b1e176
RewatkarVijay <er.vijayrewatkar@gmail.com>
2025-05-07 11:19:20 +0530

    Update code

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
-resource "aws_eip" "nat_eip" {
-  #instance = aws_instance.web-01.id
-  #domain   = "vpc"
-  network_border_group = "us-east-1"
-}
+# resource "aws_eip" "nat_eip" {
+#   #instance = aws_instance.web-01.id
+#   #domain   = "vpc"
+#   network_border_group = "us-east-1"
+# }
  
-# Create the NAT Gateway
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.q1_public_subnet.id
-  #vpc_id        = aws_vpc.main.id
-  tags = {
-    Name = "my-nat-gateway"
-  }
-}
+# # Create the NAT Gateway
+# resource "aws_nat_gateway" "nat" {
+#   allocation_id = aws_eip.nat_eip.id
+#   subnet_id     = aws_subnet.q1_public_subnet.id
+#   #vpc_id        = aws_vpc.main.id
+#   tags = {
+#     Name = "my-nat-gateway"
+#   }
+# }
  
