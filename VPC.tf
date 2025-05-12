b6dbb500d705d44fa21ddcf834956e6f5cb61f26
RewatkarVijay <er.vijayrewatkar@gmail.com>
2025-05-12 11:31:04 +0530

    Create VPC

--- VPC.tf
+++ VPC.tf
@@ -188,7 +188,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_http" {
   #to_port           = 80
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_http" {
+resource "aws_vpc_security_group_egress_rule" "allow_tls_http" {
   security_group_id = aws_security_group.allow_tls.id  
   #cidr_ipv4         = aws_vpc.main.cidr_block
   cidr_ipv4         = "0.0.0.0/0"
