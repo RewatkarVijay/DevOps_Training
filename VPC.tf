83b9bc30c7272cf5e695d638ad8a7e7624c217fd
RewatkarVijay <er.vijayrewatkar@gmail.com>
2025-05-12 11:35:28 +0530

    Create VPC

--- VPC.tf
+++ VPC.tf
@@ -179,14 +179,14 @@ resource "aws_route_table_association" "qa1_route_table_association_private_app"
 }
 
 
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_http" {
-  security_group_id = aws_security_group.allow_tls.id
-  #cidr_ipv4         = aws_vpc.main.cidr_block
-  cidr_ipv4         = "0.0.0.0/0"
-  #from_port         = 80
-  ip_protocol       = "-1"
-  #to_port           = 80
-}
+# resource "aws_vpc_security_group_ingress_rule" "allow_tls_http" {
+#   security_group_id = aws_security_group.allow_tls.id
+#   #cidr_ipv4         = aws_vpc.main.cidr_block
+#   cidr_ipv4         = "0.0.0.0/0"
+#   #from_port         = 80
+#   ip_protocol       = "-1"
+#   #to_port           = 80
+# }
 
 resource "aws_vpc_security_group_egress_rule" "allow_tls_http" {
   security_group_id = aws_security_group.allow_tls.id  
